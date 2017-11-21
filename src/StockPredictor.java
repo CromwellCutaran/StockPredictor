bd6638c835b03481861885f62988b7c078837d04
gharkamal <grewal7h@gmail.com>
2017-11-21 14:35:46 -0800

    Maven

--- src/StockPredictor.java
+++ src/StockPredictor.java
@@ -1,3 +1,6 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 public class StockPredictor {
 
