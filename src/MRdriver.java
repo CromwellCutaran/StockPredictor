5952a4531132a13a610f83e227bb31d47fd296b5
gharkamal <grewal7h@gmail.com>
2017-11-24 20:21:16 -0800

    mapper done (i think)

--- src/MRdriver.java
+++ src/MRdriver.java
@@ -10,11 +10,42 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 
 public class MRdriver extends Configured implements Tool {
+    @SuppressWarnings("deprecation")
+   public int run(String[] args) throws Exception {
+    		if(args.length !=2) {
+    			System.err.printf("usage: %s [generic options] <inputfile> <outputdir>\n", getClass().getSimpleName());
+    					 System.exit(1);
+    		}
+    		Job job;
+    		job = new Job(getConf(), "mr1" + System.getProperty("user.name"));
+    		job.setJarByClass(MRdriver.class);
+    		job.setMapperClass(MRmapper.class);
+    		job.setReducerClass(MRreducer.class); 
+    		
+    		job.setInputFormatClass(TextInputFormat.class);
+    		 job.setOutputKeyClass(Text.class);
+    		 //job.setOutputValueClass(FloatWritable.class);
+    		 job.setMapOutputValueClass(Text.class);
+    		
+    		
+    		
+    		 
+        // TODO 1: configure  MR job
+    
+        // TODO 2: setup input and output paths for MR job
+    		 FileInputFormat.addInputPath(job, new Path(args[0]));
+    		 FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-	@Override
-	public int run(String[] arg0) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+        // TODO 3: run  MR job syncronously with verbose output set to true
+    		 return job.waitForCompletion(true) ? 0 : 1;
+   }
 
+   public static void main(String[] args) throws Exception { 
+       if(args.length != 2) {
+           System.err.println("usage: MRdriver <input-path> <output-path>");
+           System.exit(1);
+       }
+       Configuration conf = new Configuration();
+       System.exit(ToolRunner.run(conf, new MRdriver(), args));
+   } 
 }
--- src/MRmapper.java
+++ src/MRmapper.java
@@ -1,6 +1,6 @@
 
 import org.apache.hadoop.mapreduce.Mapper;
-
+import org.apache.hadoop.mapreduce.Mapper.Context;
 
 //import com.sun.jersey.core.spi.scanning.Scanner;
 
@@ -10,5 +10,37 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class MRmapper  extends Mapper <LongWritable,Text,Text,Text>  {
+	
+    static String IFS=",";
+    static String OFS=",";
+    static int NF=11;
+    static int badRecordCounter =0;
+    
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        
+	
+	/*Open: The price at the beginning of the trading day
+	High: The highest price the stock reached during the day
+	Low: The lowest price reached during the day
+	Close: The final price*/
+    	Scanner input = new Scanner(value.toString().trim());  //instantiate line
+	String line = input.nextLine().trim();         //Read next line  ... trim: removes trailing spaces
+ 	String lineArray[] = line.split(",");
+ 	
+ 	String date = lineArray[0].trim();
+ 	String open = lineArray[1].trim();
+ 	String high = lineArray[2].trim();
+ 	String low = lineArray[3].trim();
+ 	String close = lineArray[4].trim();
+ 	String volume = lineArray[5].trim();
+ 	String name = lineArray[6].trim();
+ 	
+ 	String valueForKey = date + " " + open + " " + high + " " + low  + " " + close + " " + volume;
+ 	
+ 	context.write(new Text(name), new Text(valueForKey));
+ 	
+    	
+    	
+    }
 
 }
