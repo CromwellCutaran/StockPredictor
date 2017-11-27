de6a4bd8974a4ab84dca5348a6c7b133ced17331
gharkamal <grewal7h@gmail.com>
2017-11-26 23:30:45 -0800

    chart works with closing price and date

---
+++ src/Stock/LineChartDemo6.java
@@ -0,0 +1,163 @@
+package Stock;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+//import org.jfree.ui.Spacer;
+
+/**
+ * A simple demonstration application showing how to create a line chart using data from an
+ * {@link XYDataset}.
+ *
+ */
+public class LineChartDemo6 extends ApplicationFrame {
+
+    /**
+     * Creates a new demo.
+     *
+     * @param title  the frame title.
+     * @throws FileNotFoundException 
+     */
+    public LineChartDemo6(final String title) throws FileNotFoundException {
+
+        super(title);
+
+        final XYDataset dataset = createDataset();
+        final JFreeChart chart = createChart(dataset);
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+        setContentPane(chartPanel);
+
+    }
+    
+    /**
+     * Creates a sample dataset.
+     * 
+     * @return a sample dataset.
+     * @throws FileNotFoundException 
+     */
+    private XYDataset createDataset() throws FileNotFoundException {
+    	
+    	Scanner scanner = new Scanner(new File("/Users/Grewal/Desktop/text.txt"));
+
+        final XYSeries series1 = new XYSeries("Second");
+
+    	// Read the price and the living area
+        int i = 0;
+    	while (i < 10) {
+    		if (scanner.hasNextFloat()) {
+    			double livingArea = scanner.nextDouble();
+    			String price = scanner.next();
+    			if(price.charAt(0) == '0') {
+    				price = price.substring(1);
+    			}
+    			double p = Double.parseDouble(price);
+    			System.out.println(livingArea + " " +  p);
+    			series1.add(livingArea, p);
+    			i++;
+    		}
+    	}
+    	scanner.close();
+    	
+    	 
+         
+         
+        final XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series1);
+        
+        return dataset;
+        
+    }
+    
+    /**
+     * Creates a chart.
+     * 
+     * @param dataset  the data for the chart.
+     * 
+     * @return a chart.
+     */
+    private JFreeChart createChart(final XYDataset dataset) {
+        
+        // create the chart...
+        final JFreeChart chart = ChartFactory.createXYLineChart(
+            "Line Chart Demo 6",      // chart title
+            "X",                      // x axis label
+            "Y",                      // y axis label
+            dataset,                  // data
+            PlotOrientation.VERTICAL,
+            true,                     // include legend
+            true,                     // tooltips
+            false                     // urls
+        );
+
+        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+        chart.setBackgroundPaint(Color.white);
+
+//        final StandardLegend legend = (StandardLegend) chart.getLegend();
+  //      legend.setDisplaySeriesShapes(true);
+        
+        // get a reference to the plot for further customisation...
+        final XYPlot plot = chart.getXYPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+    //    plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        
+        final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesLinesVisible(0, false);
+        renderer.setSeriesShapesVisible(1, false);
+        plot.setRenderer(renderer);
+
+        // change the auto tick unit selection to integer units only...
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        // OPTIONAL CUSTOMISATION COMPLETED.
+                
+        return chart;
+        
+    }
+
+    // ****************************************************************************
+    // * JFREECHART DEVELOPER GUIDE                                               *
+    // * The JFreeChart Developer Guide, written by David Gilbert, is available   *
+    // * to purchase from Object Refinery Limited:                                *
+    // *                                                                          *
+    // * http://www.object-refinery.com/jfreechart/guide.html                     *
+    // *                                                                          *
+    // * Sales are used to provide funding for the JFreeChart project - please    * 
+    // * support us so that we can continue developing free software.             *
+    // ****************************************************************************
+    
+    /**
+     * Starting point for the demonstration application.
+     *
+     * @param args  ignored.
+     * @throws FileNotFoundException 
+     */
+    public static void main(final String[] args) throws FileNotFoundException {
+
+        final LineChartDemo6 demo = new LineChartDemo6("Stocks");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+
+    }
+
+}
+
+           
+       
---
+++ src/Stock/MRreducer.java
@@ -0,0 +1,150 @@
+package Stock;
+
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.io.Text;
+
+import java.awt.Color;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+
+public class MRreducer  extends Reducer <Text,Text,Text,Text> {
+
+	   public static String IFS=",";
+	   public static String OFS=",";
+	   public int i = 1;
+	   
+	   public void reduce(Text key, Iterable<Text> values, Context context) 
+			   throws IOException, InterruptedException {		   		   			   
+		   
+	        /** values consist of:
+	         * date
+	         * open
+	         * high
+	         * low
+	         * close
+	         * volume
+	        */
+		   //file writer 
+		 
+		   FileWriter pw = new FileWriter("data" + i +".txt");  
+		   i++;
+		   //initialize variables 
+		   String date = null;
+		   String name;
+		   double open = 0, close = 0, volume = 0;
+		   
+		   String compositeString;
+		   String[] compositeStringArray;
+		   double total = 0, first = 0, last = 0, count = 0;
+		   double previousClose = 0, previousOBV = 0, currentOBV = 0;
+		   
+		   boolean start = true;
+
+		   for(Text value: values)
+	       {
+			   
+			   if (key.toString().equals("2017")) {
+			   		System.out.println("Start of for loop: " + value);
+			   	}
+			   
+	    		compositeString = value.toString();
+	    		compositeStringArray = compositeString.split(IFS);
+
+	    		try {
+		    		date = compositeStringArray[0];
+		    		open = Double.parseDouble(compositeStringArray[1]);
+		    		close = Double.parseDouble(compositeStringArray[4]);
+		    		volume = Double.parseDouble(compositeStringArray[5]);
+		    		name = compositeStringArray[6];
+	    		} catch (Exception e) {
+	    			continue;
+	    		}
+	    		if (key.toString().equals("2017")) {
+			   		System.out.println("After try-catch: " + value);
+			   	}
+
+	    		total += (close-open)/open; // add daily change to total
+	    		count++; // increase counter
+	    		
+	    		//set first or last close values that year, 01-02 & 12-31
+	    		//for later calculating growth rate that year
+	    		
+	    		if (date.contains("2012-08-")) {
+	    			first = close;
+	    		} else if (date.contains("01-")) {
+	    			first = close;
+	    		} else if (date.contains("2017-08-")) {
+	    			last = close;
+	    		} else if (date.contains("12-")) {
+	    			last = close;
+	    		}
+	    		
+	    		/*
+	    		 * calculate the OBV -- momentum indicator that uses volume
+	    		 * flow to predict changes in stock price
+	    		 */	    		
+	    		if (start) {
+	    			start = false;
+	    			previousClose = close;
+	    		} else {
+	    			if (close > previousClose) {
+	    				currentOBV = previousOBV + volume;
+	    			} else if (close < previousClose) {
+	    				currentOBV = previousOBV - volume;	    			
+	    			} else if (close == previousClose) {
+	    				currentOBV = previousOBV;
+	    			}
+	    			
+	    			// test OBV calculation
+	    			System.out.println("volume: " + volume + "\n" +
+ 						   "previousClose: " + previousClose + "\n" +
+ 						   "close: " + close + "\n" +
+ 						   "previousOBV: " + previousOBV + "\n" +
+ 						   "currentOBV: " + currentOBV + "\n");
+	    			
+	    			previousClose = close;
+	    			previousOBV = currentOBV;
+	    			
+	    			// write currentOBV to text file
+	    			
+	    			String dateSet = date.replace("-", "").substring(4);
+	    			   
+	    	        context.write(new Text(close + " ") ,new Text(dateSet));    
+
+	    			
+	    		}	
+	    		
+	    		
+	       }
+
+//		   //calculate average daily growth rate (present-past)/past
+//		   double dailyAvg = total/count;
+//		   String dailyAvgFormatted = Double.toString(dailyAvg);
+//		   
+//		   //calculate growth rate for the year
+//		   double growthRate = (last-first)/first;
+//		   String growthRateFormatted = Double.toString(growthRate);
+//		   
+//		   Text keyTextHeader = new Text("YEAR: ");		   
+//		   context.write(keyTextHeader, key);
+//		   
+//		   Text keyTextGrowthDaily = new Text("Daily Growth Rate: ");
+//		   context.write(keyTextGrowthDaily, new Text(dailyAvgFormatted));
+//		   
+//		   Text keyTextGrowthYear = new Text("Growth Rate for Year: ");
+//		   context.write(keyTextGrowthYear, new Text(growthRateFormatted));		
+//		   
+//		   Text keyTextFooter = new Text("");
+//		   context.write(keyTextFooter, new Text(""));
+	   }
+}
