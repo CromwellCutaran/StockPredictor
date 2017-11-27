a1fe973ec2ee7a212dd3351e3857cb448aa5cb74
gharkamal <grewal7h@gmail.com>
2017-11-27 09:41:07 -0800

    fixing the chart

--- src/Stock/LineChartDemo6.java
+++ src/Stock/LineChartDemo6.java
@@ -3,21 +3,36 @@ package Stock;
 import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.DefaultKeyedValues;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.function.LineFunction2D;
+import org.jfree.data.general.DatasetUtilities;
+import org.jfree.data.statistics.Regression;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 //import org.jfree.ui.Spacer;
+
+import com.google.protobuf.TextFormat.ParseException;
 
 /**
  * A simple demonstration application showing how to create a line chart using data from an
@@ -32,16 +47,19 @@ public class LineChartDemo6 extends ApplicationFrame {
      * @param title  the frame title.
      * @throws FileNotFoundException 
      */
-    public LineChartDemo6(final String title) throws FileNotFoundException {
+
+	XYDataset inputData;
+	JFreeChart chart;
+    public LineChartDemo6(final String title) throws FileNotFoundException, java.text.ParseException {
 
         super(title);
-
+        
         final XYDataset dataset = createDataset();
-        final JFreeChart chart = createChart(dataset);
+        chart = createChart(dataset);
         final ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
         setContentPane(chartPanel);
-
+        
     }
     
     /**
@@ -49,24 +67,36 @@ public class LineChartDemo6 extends ApplicationFrame {
      * 
      * @return a sample dataset.
      * @throws FileNotFoundException 
+     * @throws java.text.ParseException 
      */
-    private XYDataset createDataset() throws FileNotFoundException {
+    private XYDataset createDataset() throws FileNotFoundException, java.text.ParseException {
     	
     	Scanner scanner = new Scanner(new File("/Users/Grewal/Desktop/text.txt"));
 
-        final XYSeries series1 = new XYSeries("Second");
+        //final XYSeries series1 = new XYSeries("Second");
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        TimeSeries series1 = new TimeSeries("Second");
+        
 
     	// Read the price and the living area
-    	while (scanner.hasNextLine()) {
+        int i = 0;
+    	while (i < 2) {
     		if (scanner.hasNextFloat()) {
     			double livingArea = scanner.nextDouble();
     			String price = scanner.next();
+    			 SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+    		        String dateInString = price;
+
+    		        java.util.Date date = formatter.parse(dateInString);
+					System.out.println(date);
+					System.out.println(formatter.format(date));
     			if(price.charAt(0) == '0') {
     				price = price.substring(1);
     			}
     			double p = Double.parseDouble(price);
     			System.out.println(livingArea + " " +  p);
-    			series1.add(p, livingArea);
+    			series1.add(new Day(date), livingArea);
+    			i++;
     		}
     	}
     	scanner.close();
@@ -74,9 +104,9 @@ public class LineChartDemo6 extends ApplicationFrame {
     	 
          
          
-        final XYSeriesCollection dataset = new XYSeriesCollection();
+         dataset = new TimeSeriesCollection();
         dataset.addSeries(series1);
-        
+        inputData = dataset;
         return dataset;
         
     }
@@ -128,6 +158,30 @@ public class LineChartDemo6 extends ApplicationFrame {
         return chart;
         
     }
+    
+	private void drawRegressionLine() {
+		// Get the parameters 'a' and 'b' for an equation y = a + b * x,
+		// fitted to the inputData using ordinary least squares regression.
+		// a - regressionParameters[0], b - regressionParameters[1]
+		double regressionParameters[] = Regression.getOLSRegression(inputData,
+				0);
+
+		// Prepare a line function using the found parameters
+		LineFunction2D linefunction2d = new LineFunction2D(
+				regressionParameters[0], regressionParameters[1]);
+
+		// Creates a dataset by taking sample values from the line function
+		XYDataset dataset = DatasetUtilities.sampleFunction2D(linefunction2d,
+				800D, 950, 100, "Fitted Regression Line");
+
+		// Draw the line dataset
+		XYPlot xyplot = chart.getXYPlot();
+		xyplot.setDataset(1, dataset);
+		XYLineAndShapeRenderer xylineandshaperenderer = new XYLineAndShapeRenderer(
+				true, false);
+		xylineandshaperenderer.setSeriesPaint(0, Color.YELLOW);
+		xyplot.setRenderer(1, xylineandshaperenderer);
+	}
 
     // ****************************************************************************
     // * JFREECHART DEVELOPER GUIDE                                               *
@@ -145,16 +199,60 @@ public class LineChartDemo6 extends ApplicationFrame {
      *
      * @param args  ignored.
      * @throws FileNotFoundException 
+     * @throws java.text.ParseException 
+     * @throws java.text.ParseException 
      */
-    public static void main(final String[] args) throws FileNotFoundException {
+	 public static void main(String args[]) throws FileNotFoundException, java.text.ParseException{
 
-        final LineChartDemo6 demo = new LineChartDemo6("Stocks");
-        demo.pack();
-        RefineryUtilities.centerFrameOnScreen(demo);
-        demo.setVisible(true);
+	        DefaultKeyedValues data = new DefaultKeyedValues();
+	        
+	     	Scanner scanner = new Scanner(new File("/Users/Grewal/Desktop/text.txt"));
 
-    }
+	        int i = 0;
+	    	while (i < 10) {
+	    		if (scanner.hasNextFloat()) {
+	    			double livingArea = scanner.nextDouble();
+	    			String price = scanner.next();
+	    			 SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+	    		        String dateInString = price;
 
+	    		        java.util.Date date = formatter.parse(dateInString);
+						//System.out.println(date);
+						System.out.println(formatter.format(date) + " "+ livingArea);
+	    			if(price.charAt(0) == '0') {
+	    				price = price.substring(1);
+	    			}
+	    			//double p = Double.parseDouble(price);
+	    			//System.out.println(livingArea + " " +  p);
+	    			data.addValue(formatter.format(date), livingArea);
+	    			i++;
+	    		}
+	    	}
+	    	scanner.close();
+	    	
+	        
+	        
+	        
+	        
+	        //data.addValue("8/4/2012" ,7.0);
+	        //data.addValue("19/04/2012",5.0);
+
+	        CategoryDataset dataset = DatasetUtilities.createCategoryDataset("Stock", data);
+	        JFreeChart chart = ChartFactory.createBarChart("Stock","Date","OBV",dataset,PlotOrientation.VERTICAL,true,true,true);
+	        ChartFrame frame = new ChartFrame("Test", chart);
+
+	        //Switch from a Bar Rendered to a LineAndShapeRenderer so the chart looks like an XYChart
+	        LineAndShapeRenderer renderer = new LineAndShapeRenderer();
+	        renderer.setBaseLinesVisible(false); //TUrn of the lines
+	        CategoryPlot plot = (CategoryPlot) chart.getPlot();
+	        plot.setRenderer(0, renderer);
+
+	        NumberAxis numberAxis = (NumberAxis)plot.getRangeAxis();        
+	        numberAxis.setRange(-10000000,10000000);   
+	        data.setVerticalTickLabels(true);
+	        frame.pack();
+	        frame.setVisible(true);
+	    }  
 }
 
            
